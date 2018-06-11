0560c79019e114456ce2745dd7ea9814293fe584
Adimilson Filho <adimilsonfilho10@hotmail.com>
2018-06-11 18:52:01 -0300

    Primeira versão

---
+++ ola_git.php
@@ -0,0 +1,12 @@
+1. Selecionar o local
+
+cd c:/projeto_integrador
+
+2. Inicializar um repositório
+git init
+
+
+
+3.Setar as minhas configurações
+$ git config --global user.email "adimilsonfilho10@hotmail.com"
+$ git config --global user.name "Adimilson Filho"
