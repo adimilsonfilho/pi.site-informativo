3ccd8373d1c1184fc1b393fd7986048366e3bc82
Adimilson Filho <adimilsonfilho10@hotmail.com>
2018-06-11 21:36:50 -0300

    Primeira versão

--- ola_git.php
+++ ola_git.php
@@ -1,12 +1,37 @@
 1. Selecionar o local
-
 cd c:/projeto_integrador
 
 2. Inicializar um repositório
 git init
 
+3. Setar as minhas configurações
+git config --global user.name "Adimilson Filho"
+git config --global user.email "adimilsonfilho10@hotmail.com"
 
 
-3.Setar as minhas configurações
-$ git config --global user.email "adimilsonfilho10@hotmail.com"
-$ git config --global user.name "Adimilson Filho"
+4. Setar as configurações do servidor
+git remote add origin https://github.com/adimilsonfilho/pi.site-informativo.git
+
+
+5. Criar uma versão
+git add.
+git commit -m "Primeira versão"
+git push -u origin master
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$ git push -u origin master ( fazer login )
